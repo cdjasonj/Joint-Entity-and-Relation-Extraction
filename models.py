04031632712ddeee3c74d42e9a50d84f88380f50
cdjasonj <419176558@qq.com>
2019-05-27 20:10:34 +0800

    first commit

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ from keras.models import Model
 from keras import backend as K
 
 class base_model():
-    def __init__(self,hidden_size,embed_size,vocab_size,maxlen,num_classes_part1,num_classes_part2,dropout_prob=0.1):
+    def __init__(self,hidden_size,embed_size,vocab_size,multi_layers,maxlen,num_classes_part1,num_classes_part2,
+                 learning_rate = 5e-5,dropout_prob=0.1,optmizer='adam'):
         """
         base multi-head selection joint extracton model
         :param hidden_size:
@@ -20,6 +21,8 @@ class base_model():
         self.embed_size = embed_size
         self.vocab_size = vocab_size
         self.maxlen = maxlen
+        self.multi_layers = multi_layers
+        self.learning_rate = learning_rate
         self.dropout_prob = dropout_prob
 
     def model(self):
@@ -37,24 +40,27 @@ class base_model():
 
         if self.dropout_prob:
             word_embedding = Dropout(self.dropout_prob)(word_embedding)
-        #lstm layer
-        lstm1 = Bidirectional(CuDNNLSTM(self.hidden_size//2,return_sequences=True,name='lstm_layer1'))(word_embedding)
-        lstm2 = Bidirectional(CuDNNLSTM(self.hidden_size//2,return_sequences=True,name='lstm_layer2'))(lstm1)
 
-        attention = TimeDistributed(Dense(1, activation='tanh'))(lstm2)
+        lstm = Bidirectional(CuDNNLSTM(self.hidden_size // 2, return_sequences=True, name='lstm_layer0'))(word_embedding)
+        #multi_lstm_layers
+        if self.multi_layers >= 2:
+            for i in range(self.multi_layers -1 ):
+                lstm = Bidirectional(CuDNNLSTM(self.hidden_size//2,return_sequences=True,name='lstm_layer{}'.format(i+1)))(lstm)
+
+        attention = TimeDistributed(Dense(1, activation='tanh'))(lstm)
         attention = Flatten()(attention)
         attention = Activation('softmax')(attention)
         attention = RepeatVector(self.hidden_size)(attention)
         attention = Permute([2, 1])(attention)
-        sent_representation = multiply([lstm2, attention])
+        sent_representation = multiply([lstm, attention])
         attention = Lambda(lambda xin: K.sum(xin, axis=1))(sent_representation)
-        lstm_attention = Lambda(seq_and_vec, output_shape=(None, self.hidden_size * 2))([lstm2, attention]) #[这里考虑下用相加的方法，以及门控相加]
+        lstm_attention = Lambda(seq_and_vec, output_shape=(None, self.hidden_size * 2))([lstm, attention]) #[这里考虑下用相加的方法，以及门控相加]
         entity_pred = Dense(self.num_classes_part1,activation='softmax')(lstm_attention)
         entity_model = Model([word_input],[entity_pred])
 
         ###############################################################################################################################
         #part2 multi-head selection for relation classfication
-        h = Concatenate(axis=-1)([lstm2,entity_pred])
+        h = Concatenate(axis=-1)([lstm,entity_pred])
         multi_head_selection_pred = Dense(self.num_classes_part2,activation='softmax')(h)
         relation_model = Model([word_input],[multi_head_selection_pred])
         train_model = Model([word_input,outputs_part1,outputs_part2],[multi_head_selection_pred])
@@ -66,6 +72,7 @@ class base_model():
 
         train_model.add_loss(part1_loss+part2_loss)
         train_model.compile(keras.optimizers.adam(lr=5e-5))
+
         return entity_model,relation_model,train_model
 
 class word_char_model_1():
